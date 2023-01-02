7ce3f6b6a6d5c8e2f557b9dad525685c35021eb2
Gene Kelley <phpcodehead@gmail.com>
2023-01-02 12:54:25 -0700

    Mount App and add product and description data properties.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {                   // <== ES6 Shorthand for: data: function () ...
+        return {
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks.'
+        }
+    }
+})
