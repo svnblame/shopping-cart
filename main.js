d1433d6a9cde97afbaf0e5d2fe42f3f5b005536c
Gene Kelley <phpcodehead@gmail.com>
2023-01-02 15:42:15 -0700

    Add rendered lists.

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 8
+            inventory: 8,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'Green'},
+                { id: 2235, color: 'Blue'}
+            ],
+            sizes: [
+                { id: 1, size: 'Small' },
+                { id: 2, size: 'Medium' },
+                { id: 3, size: 'Large' }
+            ]
         }
     }
 })
